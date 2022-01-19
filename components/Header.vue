654781b83ebcbe62029a2b6edcb79cd1ffa1f142
guido offermans <guidooffermans@hotmail.com>
2022-01-19 23:19:18 +0100

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <h1>title hello hello 2</h1>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>header 2</h1>
+    <p v-if="name">{{name}}</p>
+	<p v-else>no name</p>
+    <Header />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    const name = "";
+
+    return { name };
+  },
+});
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
